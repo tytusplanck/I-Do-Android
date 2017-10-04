7043d900317d28acdbe6e8b302b87a6259037acc
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2017-10-04 17:45:10 -0400

    Yeah boi we got some activities up in this bizness

---
+++ app/src/main/java/com/example/tyle/ido/UserProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.tyle.ido;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tytusplanck.ido.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
