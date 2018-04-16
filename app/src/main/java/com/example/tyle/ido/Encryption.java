ecff97b676001a6c75e5df63d94c6cc6a3a4660e
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2018-04-16 00:20:08 -0400

    Added RC4 encryption

--- app/src/main/java/com/example/tyle/ido/Encryption.java
+++ app/src/main/java/com/example/tyle/ido/Encryption.java
@@ -1,84 +1,46 @@
 package com.example.tyle.ido;
 
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
-import android.support.annotation.NonNull;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.UnrecoverableEntryException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.util.Arrays;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.GCMParameterSpec;
-
-/**
- *
- *
- * This class contains methods to encrypt Strings,
- * generates and retrieves the key to encrypt with AES 256.
- */
-
 public class Encryption {
 
+    private final byte[] S = new byte[256];
+    private final byte[] T = new byte[256];
+    private final int keylen;
 
-    private static final int SBOX_LENGTH = 256;
-    private static final int KEY_MIN_LENGTH = 5;
-    /**
-     * Key array
-     */
-    private byte[] key = new byte[SBOX_LENGTH - 1];
-    /**
-     * Sbox
-     */
-    private int[] sbox = new int[SBOX_LENGTH];
+    public Encryption(final byte[] key) {
 
-
-    /**
-     * Creates a cipher and encrypts the String passed according to that cipher.
-     *
-     * @param textToEncrypt
-     * @return
-     *
-     */
-    public byte[] encryptText(final String textToEncrypt)
-            throws Exception {
-
-        return new byte[0];
+            keylen = key.length;
+            for (int i = 0; i < 256; i++) {
+                S[i] = (byte) i;
+                T[i] = key[i % keylen];
+            }
+            int j = 0;
+            byte storage;
+            for (int i = 0; i < 256; i++) {
+                j = (j + S[i] + T[i]) & 0xFF;
+                storage = S[j];
+                S[j] = S[i];
+                S[i] = storage;
+            }
     }
 
+    public byte[] encrypt(final byte[] textToEncrypt) {
+        final byte[] encryptedText = new byte[textToEncrypt.length];
+        int i = 0, j = 0, k, t;
+        byte tmp;
+        for (int counter = 0; counter < textToEncrypt.length; counter++) {
+            i = (i + 1) & 0xFF;
+            j = (j + S[i]) & 0xFF;
+            tmp = S[j];
+            S[j] = S[i];
+            S[i] = tmp;
+            t = (S[i] + S[j]) & 0xFF;
+            k = S[t];
+            encryptedText[counter] = (byte) (textToEncrypt[counter] ^ k);
+        }
+        return encryptedText;
+    }
 
-    /**
-     * Decrypts a string of encrypted data
-     *
-     * @param encryptedDataWithIV
-     * @return - the decrypted string
-     * @throws UnrecoverableEntryException
-     * @throws NoSuchAlgorithmException
-     * @throws KeyStoreException
-     * @throws NoSuchProviderException
-     * @throws NoSuchPaddingException
-     * @throws InvalidKeyException
-     * @throws IOException
-     * @throws BadPaddingException
-     * @throws IllegalBlockSizeException
-     * @throws InvalidAlgorithmParameterException
-     * @throws CertificateException
-     */
-    public String decryptText(final byte[] encryptedDataWithIV) {
-        return "skrrt";
+    public byte[] decrypt(final byte[] ciphertext) {
+        return encrypt(ciphertext);
     }
 }
